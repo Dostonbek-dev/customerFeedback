ae3ae466a661af9458958875420dd7d611140eb2
dostonbekkhamidov@gmail.com <dostonbekkhamidov@gmail.com>
2022-05-31 19:25:59 +0500

    last changes

---
+++ src/main/java/uz/davr/dto/response/EmpDto.java
@@ -0,0 +1,15 @@
+package uz.davr.dto.response;
+
+import lombok.Data;
+
+@Data
+public class EmpDto {
+    private String firstname;
+    private String lastname;
+    private String parentName;
+    private String positions;
+    private String phone;
+
+}
+
+
